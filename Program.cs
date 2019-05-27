e2a44a937bd5e1cc27d72b85a9be2ef6af9338e5
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2019-05-27 22:49:06 +0900

    精進

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AtCoder
     {
         static void Main(string[] args)
         {
-            string TARGET = "ABC128";
+            string TARGET = "ABC120";
 
             var ts = Assembly.GetExecutingAssembly().GetTypes();
             foreach (var item in ts)
