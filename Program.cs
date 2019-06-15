3be013537ad8966b00cd84a32df7c73a09cd0a20
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2019-06-16 00:06:17 +0900

    Diverta2019-2　3完

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AtCoder
     {
         static void Main(string[] args)
         {
-            string TARGET = "ABC113";
+            string TARGET = "DIVERTA2019_2";
 
             var ts = Assembly.GetExecutingAssembly().GetTypes();
             foreach (var item in ts)
