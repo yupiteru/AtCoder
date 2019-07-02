f467a667d96dbd6e73b69b6250b4c2cabdc7467b
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2019-07-03 00:20:13 +0900

    赤黒木実装のテスト

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AtCoder
     {
         static void Main(string[] args)
         {
-            string TARGET = "ABC103";
+            string TARGET = "ABC128";
 
             var ts = Assembly.GetExecutingAssembly().GetTypes();
             foreach (var item in ts)
