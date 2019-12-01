0e0d7a8f984586f57ba160050291d4673638a90f
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2019-12-01 23:54:30 +0900

    全完

---
+++ other/sumitrust2019/SUMITRUST2019F.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Math;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Library;
+
+namespace Program
+{
+    public static class SUMITRUST2019F
+    {
+        static public int numberOfRandomCases = 0;
+        static public void MakeTestCase(List<string> _input, List<string> _output, ref Func<string[], bool> _outputChecker)
+        {
+        }
+        static public void Solve()
+        {
+            var T1 = NN;
+            var T2 = NN;
+            var A1 = NN;
+            var A2 = NN;
+            var B1 = NN;
+            var B2 = NN;
+            if (A1 < B1)
+            {
+                {
+                    var t = A1; A1 = B1; B1 = t;
+
+                }
+                {
+                    var t = A2; A2 = B2; B2 = t;
+
+                }
+            }
+            var sa1 = A1 - B1;
+            var sa2 = A2 - B2;
+            var hanare1 = T1 * sa1;
+            var hanare2 = T2 * sa2;
+            if (sa1 > 0)
+            {
+                if (hanare1 + hanare2 > 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    var dif = hanare1 + hanare2;
+                    if (dif == 0)
+                    {
+                        Console.WriteLine("infinity");
+                        return;
+                    }
+                    var cnt = hanare1 / -dif;
+                    var adj = 0;
+                    if (hanare1 % dif == 0) adj = 1;
+                    Console.WriteLine(1 + cnt * 2 - adj);
+                }
+            }
+        }
+        static class Console_
+        {
+            static Queue<string> param = new Queue<string>();
+            public static string NextString() { if (param.Count == 0) foreach (var item in Console.ReadLine().Split(' ')) param.Enqueue(item); return param.Dequeue(); }
+        }
+        class Printer : StreamWriter
+        {
+            public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+            public Printer(Stream stream) : base(stream, new UTF8Encoding(false, true)) { base.AutoFlush = false; }
+            public Printer(Stream stream, Encoding encoding) : base(stream, encoding) { base.AutoFlush = false; }
+        }
+        static public void Main(string[] args) { if (args.Length == 0) { Console.SetOut(new Printer(Console.OpenStandardOutput())); } var t = new Thread(Solve, 134217728); t.Start(); t.Join(); Console.Out.Flush(); }
+        static long NN => long.Parse(Console_.NextString());
+        static double ND => double.Parse(Console_.NextString());
+        static string NS => Console_.NextString();
+        static long[] NNList(long N) => Repeat(0, N).Select(_ => NN).ToArray();
+        static double[] NDList(long N) => Repeat(0, N).Select(_ => ND).ToArray();
+        static string[] NSList(long N) => Repeat(0, N).Select(_ => NS).ToArray();
+        static IEnumerable<T> OrderByRand<T>(this IEnumerable<T> x) => x.OrderBy(_ => xorshift);
+        static long Count<T>(this IEnumerable<T> x, Func<T, bool> pred) => Enumerable.Count(x, pred);
+        static IEnumerable<T> Repeat<T>(T v, long n) => Enumerable.Repeat<T>(v, (int)n);
+        static IEnumerable<int> Range(long s, long c) => Enumerable.Range((int)s, (int)c);
+        static uint xorshift { get { _xsi.MoveNext(); return _xsi.Current; } }
+        static IEnumerator<uint> _xsi = _xsc();
+        static IEnumerator<uint> _xsc() { uint x = 123456789, y = 362436069, z = 521288629, w = (uint)(DateTime.Now.Ticks & 0xffffffff); while (true) { var t = x ^ (x << 11); x = y; y = z; z = w; w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); yield return w; } }
+    }
+}
