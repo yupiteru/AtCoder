6e8034747f1f6889903f60a785bed4be1abe0660
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-03-01 22:50:11 +0900

    ABC157

---
+++ ABC/ABC157/ABC157B.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Math;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Library;
+
+namespace Program
+{
+    public static class ABC157B
+    {
+        static public int numberOfRandomCases = 0;
+        static public void MakeTestCase(List<string> _input, List<string> _output, ref Func<string[], bool> _outputChecker)
+        {
+        }
+        static public void Solve()
+        {
+            var AList1 = NNList(3);
+            var AList2 = NNList(3);
+            var AList3 = NNList(3);
+            var N = NN;
+            var bList = NNList(N);
+            var ACList1 = AList1.Select(e => bList.Contains(e)).ToArray();
+            var ACList2 = AList2.Select(e => bList.Contains(e)).ToArray();
+            var ACList3 = AList3.Select(e => bList.Contains(e)).ToArray();
+            var ok = false;
+            ok |= ACList1.All(e => e);
+            ok |= ACList2.All(e => e);
+            ok |= ACList3.All(e => e);
+            ok |= ACList1[0] && ACList2[0] && ACList3[0];
+            ok |= ACList1[1] && ACList2[1] && ACList3[1];
+            ok |= ACList1[2] && ACList2[2] && ACList3[2];
+            ok |= ACList1[0] && ACList2[1] && ACList3[2];
+            ok |= ACList1[2] && ACList2[1] && ACList3[0];
+            if (ok) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+        static class Console_
+        {
+            static Queue<string> param = new Queue<string>();
+            public static string NextString() { if (param.Count == 0) foreach (var item in Console.ReadLine().Split(' ')) param.Enqueue(item); return param.Dequeue(); }
+        }
+        class Printer : StreamWriter
+        {
+            public override IFormatProvider FormatProvider { get { return CultureInfo.InvariantCulture; } }
+            public Printer(Stream stream) : base(stream, new UTF8Encoding(false, true)) { base.AutoFlush = false; }
+            public Printer(Stream stream, Encoding encoding) : base(stream, encoding) { base.AutoFlush = false; }
+        }
+        static public void Main(string[] args) { if (args.Length == 0) { Console.SetOut(new Printer(Console.OpenStandardOutput())); } var t = new Thread(Solve, 134217728); t.Start(); t.Join(); Console.Out.Flush(); }
+        static long NN => long.Parse(Console_.NextString());
+        static double ND => double.Parse(Console_.NextString());
+        static string NS => Console_.NextString();
+        static long[] NNList(long N) => Repeat(0, N).Select(_ => NN).ToArray();
+        static double[] NDList(long N) => Repeat(0, N).Select(_ => ND).ToArray();
+        static string[] NSList(long N) => Repeat(0, N).Select(_ => NS).ToArray();
+        static IEnumerable<T> OrderByRand<T>(this IEnumerable<T> x) => x.OrderBy(_ => xorshift);
+        static long Count<T>(this IEnumerable<T> x, Func<T, bool> pred) => Enumerable.Count(x, pred);
+        static IEnumerable<T> Repeat<T>(T v, long n) => Enumerable.Repeat<T>(v, (int)n);
+        static IEnumerable<int> Range(long s, long c) => Enumerable.Range((int)s, (int)c);
+        static IEnumerable<string> OrderBy(this IEnumerable<string> x) => x.OrderBy(e => e, StringComparer.OrdinalIgnoreCase);
+        static IEnumerable<T> OrderBy<T>(this IEnumerable<T> x, Func<T, string> selector) => x.OrderBy(selector, StringComparer.OrdinalIgnoreCase);
+        static IEnumerable<string> OrderByDescending(this IEnumerable<string> x) => x.OrderByDescending(e => e, StringComparer.OrdinalIgnoreCase);
+        static IEnumerable<T> OrderByDescending<T>(this IEnumerable<T> x, Func<T, string> selector) => x.OrderByDescending(selector, StringComparer.OrdinalIgnoreCase);
+        static uint xorshift { get { _xsi.MoveNext(); return _xsi.Current; } }
+        static IEnumerator<uint> _xsi = _xsc();
+        static IEnumerator<uint> _xsc() { uint x = 123456789, y = 362436069, z = 521288629, w = (uint)(DateTime.Now.Ticks & 0xffffffff); while (true) { var t = x ^ (x << 11); x = y; y = z; z = w; w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); yield return w; } }
+    }
+}
