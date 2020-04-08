4eee34ef235021c34d4ed07656c1c4b766d59848
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-04-08 21:10:13 +0900

    update lib

--- lib/Geo2D.cs
+++ lib/Geo2D.cs
@@ -13,7 +13,7 @@ namespace Library
     ////start
     class LIB_Geo2D
     {
-        static readonly double EPS = 1e-10;
+        static readonly public double EPS = 1e-10;
         public struct Vec
         {
             public double x;
@@ -50,6 +50,41 @@ namespace Library
             }
         }
         /// <summary>
+        /// 点と点の距離の2乗を求める
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public double Distance(Vec p1, Vec p2) => Norm(p2 - p1);
+        /// <summary>
+        /// 点と円の距離の2乗を求める。負の場合は円の内側にいる（絶対値は一番近い円の縁までの距離）
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public double Distance(Vec p, Circle c) => Norm(p - c.p) - c.r2;
+        /// <summary>
+        /// 点と円の距離の2乗を求める。負の場合は円の内側にいる（絶対値は一番近い円の縁までの距離）
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public double Distance(Circle c, Vec p) => Norm(p - c.p) - c.r2;
+        /// <summary>
+        /// 円と円の交点を求める
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public Vec[] Intersection(Circle c1, Circle c2)
+        {
+            var p = c2.p - c1.p;
+            var b = p.x * p.x + p.y * p.y;
+            var a = (b + c1.r2 - c2.r2) / 2;
+            var d = b * c1.r2 - a * a;
+            if (d < -EPS) return new Vec[0];
+            if (d > EPS)
+            {
+                var c = Sqrt(d);
+                var p1 = new Vec((a * p.x + p.y * c) / b + c1.p.x, (a * p.y - p.x * c) / b + c1.p.y);
+                var p2 = new Vec((a * p.x - p.y * c) / b + c1.p.x, (a * p.y + p.x * c) / b + c1.p.y);
+                return new[] { p1, p2 };
+            }
+            return new[] { new Vec(a * p.x / b + c1.p.x, a * p.y / b + c1.p.y) };
+        }
+        /// <summary>
         /// pの長さの2乗を得る
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -70,12 +105,12 @@ namespace Library
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static public Circle GetCircleFrom3Vec(Vec p1, Vec p2, Vec p3)
         {
-            var A = Norm(p2 - p3);
-            var B = Norm(p3 - p1);
-            var C = Norm(p1 - p2);
+            var A = Distance(p2, p3);
+            var B = Distance(p1, p3);
+            var C = Distance(p1, p2);
             var S = Abs(Cross(p2 - p1, p3 - p1));
             var p = (A * (B + C - A) * p1 + B * (C + A - B) * p2 + C * (A + B - C) * p3) / (4 * S * S);
-            var r = Norm(p - p1);
+            var r = Distance(p, p1);
             return new Circle() { p = p, r2 = r };
         }
         /// <summary>
@@ -85,7 +120,7 @@ namespace Library
         static public Circle GetCircleFrom2Vec(Vec p1, Vec p2)
         {
             var c = (p1 + p2) / 2;
-            return new Circle() { p = c, r2 = Norm(p1 - c) };
+            return new Circle() { p = c, r2 = Distance(p1, c) };
         }
         /// <summary>
         /// 最小包含円を返す
@@ -98,7 +133,7 @@ namespace Library
             var rnd = new Random();
             var list = pList.OrderBy(_ => rnd.Next()).ToArray();
             Func<Vec, Circle, bool> inCircle = null;
-            inCircle = (p, c) => Norm(p - c.p) <= c.r2 + EPS;
+            inCircle = (p, c) => Distance(p, c) < EPS;
             {
                 var c = GetCircleFrom2Vec(list[0], list[1]);
                 for (var i = 2; i < list.Length; i++)
