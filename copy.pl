d23446cbfcb76be5d901f158143902762fadb9d0
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-05-04 17:14:39 +0900

    libフォルダを再帰的に読むように変更

--- copy.pl
+++ copy.pl
@@ -1,43 +1,56 @@
 use strict;
 use warnings;
 use Win32::Clipboard;
+use Encode qw/encode decode/;
 
-opendir my $dh, "lib";
 my %library;
 my %classToFilename;
 my %filenameToUsedLib;
-while(my $libfile = readdir $dh) {
-  if($libfile =~ /\.cs$/) {
-    open my $libfh, "<lib\\$libfile";
-    my $libstr = "";
-    my $copyStarted = 0;
-    while(my $line = <$libfh>) {
-      if($copyStarted == 1) {
-        if($line =~ m|////end|) {
-          last;
-        }
-        if($line =~ /(class|struct) LIB_([A-Za-z0-9_]+)/) {
-          $classToFilename{$2} = $libfile;
-        }
-        if($line =~ /LIB_([A-Za-z0-9_]+)/) {
-          ${$filenameToUsedLib{$libfile}}[@{$filenameToUsedLib{$libfile}}] = $1;
-        }
-        $libstr .= $line;
-      }elsif($line =~ m|////start|) {
-        $copyStarted = 1;
-      }
+
+my @folders = ("lib");
+while(@folders > 0) {
+  my $dir = pop @folders;
+  opendir my $dh, $dir or die;
+  while(my $libfile = readdir $dh) {
+    my $path = "$dir\\$libfile";
+    if($libfile eq "." or $libfile eq "..") {
+      next;
     }
-    close $libfh;
-    $library{$libfile} = $libstr;
+    elsif(-d $path) {
+      push @folders, $path;
+    }
+    elsif($libfile =~ /\.cs$/) {
+      open my $libfh, "<$path";
+      my $libstr = "";
+      my $copyStarted = 0;
+      while(my $line = decode('UTF-8', <$libfh>)) {
+        if($copyStarted == 1) {
+          if($line =~ m|////end|) {
+            last;
+          }
+          if($line =~ /(class|struct) LIB_([A-Za-z0-9_]+)/) {
+            $classToFilename{$2} = $path;
+          }
+          if($line =~ /LIB_([A-Za-z0-9_]+)/) {
+            ${$filenameToUsedLib{$path}}[@{$filenameToUsedLib{$path}}] = $1;
+          }
+          $libstr .= $line;
+        }elsif($line =~ m|////start|) {
+          $copyStarted = 1;
+        }
+      }
+      close $libfh;
+      $library{$path} = $libstr;
+    }
   }
+  closedir $dh;
 }
-closedir $dh;
 
 my $filename = $ARGV[0];
 open my $fh, "<$filename";
 my $str = "";
 my %usedLib;
-while(my $line = <$fh>) {
+while(my $line = decode('UTF-8', <$fh>)) {
   $str .= $line;
   while($line =~ /LIB_([A-Za-z0-9_]+)/g) {
     my $libFileName = $classToFilename{$1};
@@ -65,5 +78,5 @@ foreach my $item (keys(%usedLib)) {
 }
 $str .= "}\r\n";
 
-Win32::Clipboard()->Set($str);
+Win32::Clipboard()->Set(encode('Shift_JIS', $str));
 
