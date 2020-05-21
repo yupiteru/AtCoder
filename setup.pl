365bacb170c93ddd36576dd6f1c7c0d47f77f71a
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-05-22 02:15:54 +0900

    提出コードの保存をやめた

---
+++ setup.pl
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+use File::Path 'remove_tree';
+
+my $limit = uc <STDIN>;
+$limit =~ s/[\r\n]+//;
+if(length $limit > 1 or !$limit =~ /[A-Z]/) {
+  $limit = "Z";
+}
+my $baseFolder = "problems";
+
+if(-e $baseFolder) {
+  remove_tree $baseFolder;
+}
+mkdir $baseFolder;
+
+open my $fh, "<template\\template.cs";
+my @data;
+while(my $line = <$fh>) {
+  $data[@data] = $line;
+}
+close $fh;
+
+open $fh, "<template\\template_TestCase.txt";
+my @dataCase;
+while(my $line = <$fh>) {
+  $dataCase[@dataCase] = $line;
+}
+close $fh;
+
+for(my $i = 'A';length $i < 2 and $i le $limit; $i++) {
+  open $fh, ">$baseFolder\\Problem$i.cs";
+  foreach my $line (@data) {
+    if($line =~ /class template/) {
+      my $tmp = $line;
+      $tmp =~ s/class template/class Problem$i/;
+      print $fh $tmp;
+    } else {
+      print $fh $line;
+    }
+  }
+  close $fh;
+
+  open $fh, ">$baseFolder\\Problem${i}_TestCase.txt";
+  foreach my $line (@dataCase) {
+    print $fh $line;
+  }
+  close $fh;
+}
