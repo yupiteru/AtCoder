70f46a1fd80f99f083c0c033e5f663e8a520b271
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-06-30 02:05:55 +0900

    update lib: stirling second

--- lib/Mod/StirlingNumber.cs
+++ lib/Mod/StirlingNumber.cs
@@ -14,14 +14,17 @@ namespace Library
     /// copy key class LIB_StirlingSecond
     partial struct /* not copy key */ LIB_Mod
     {
+        /// <summary>
+        /// S(n, 0), S(n, 1), S(n, 2) ... S(n, k)
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static public LIB_Mod[] LIB_StirlingSecond(long n)
+        static public LIB_Mod[] LIB_StirlingSecond(long n, long k)
         {
-            var a = new LIB_Mod[n + 1];
-            for (var i = 0; i <= n; i++) a[i] = (i % 2 == 1 ? -1 : 1) / Perm(i, i);
-            var b = new LIB_Mod[n + 1];
-            for (var i = 0; i <= n; i++) b[i] = Pow(i, n) / Perm(i, i);
-            return LIB_NTT.Multiply(a.Select(e => (long)e).ToArray(), b.Select(e => (long)e).ToArray(), _mod).Take((int)n + 1).Select(e => (LIB_Mod)e).ToArray();
+            var a = new LIB_Mod[k + 1];
+            for (var i = 0; i <= k; i++) a[i] = (i % 2 == 1 ? -1 : 1) / Perm(i, i);
+            var b = new LIB_Mod[k + 1];
+            for (var i = 0; i <= k; i++) b[i] = Pow(i, n) / Perm(i, i);
+            return LIB_NTT.Multiply(a.Select(e => (long)e).ToArray(), b.Select(e => (long)e).ToArray(), _mod).Take((int)k + 1).Select(e => (LIB_Mod)e).ToArray();
         }
     }
     ////end
