d5de0acd7adfa44c140192b20637a38d3074ec32
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-09-01 21:44:18 +0900

    update lib: Union Find

--- lib/UnionFind.cs
+++ lib/UnionFind.cs
@@ -11,20 +11,16 @@ using System.Runtime.CompilerServices;
 namespace Library
 {
     ////start
-    class LIB_UnionFind
+    class LIB_UnionFind<T>
     {
-        long[] d;
-        Func<long, long, long> f;
+        int[] d;
+        T[] v;
+        Func<T, T, T> f;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public LIB_UnionFind(long s)
+        public LIB_UnionFind(IEnumerable<T> s, Func<T, T, T> func)
         {
-            d = Enumerable.Repeat(-1L, (int)s).ToArray();
-            f = (x, y) => x + y;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public LIB_UnionFind(IEnumerable<long> s, Func<long, long, long> func)
-        {
-            d = s.Select(e => -e).ToArray();
+            v = s.ToArray();
+            d = Enumerable.Repeat(-1, v.Length).ToArray();
             f = func;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -34,17 +30,27 @@ namespace Library
             y = Root(y);
             if (x != y)
             {
-                d[x] = -f(-d[x], -d[y]);
-                d[y] = x;
+                d[x] = d[x] + d[y];
+                v[x] = f(v[x], v[y]);
+                d[y] = (int)x;
             }
             return x != y;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool IsSame(long x, long y) => Root(x) == Root(y);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public long Root(long x) => d[x] < 0 ? x : d[x] = Root(d[x]);
+        public long Root(long x) => d[x] < 0 ? x : d[x] = (int)Root(d[x]);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public long Calc(long x) => -d[Root(x)];
+        public long Count(long x) => -d[Root(x)];
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T Calc(long x) => v[Root(x)];
+    }
+    class LIB_UnionFind : LIB_UnionFind<int>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public LIB_UnionFind(long n) : base(Enumerable.Repeat(0, (int)n), (x, y) => x + y) { }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        new private void Calc(long x) { }
     }
     ////end
 }
