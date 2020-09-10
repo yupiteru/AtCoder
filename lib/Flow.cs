c7e2154eb140d4ee11aecca2c73be608bd400d73
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-09-11 01:05:24 +0900

    update lib: fix dinic

--- lib/Flow.cs
+++ lib/Flow.cs
@@ -66,6 +66,7 @@ namespace Library
         {
             var f = Enumerable.Repeat(0, N).Select(_ => new LIB_Dict<int, long>()).ToArray();
             for (var i = 0; i < N; i++) foreach (var item in path[i]) f[i][item.Key] = item.Value;
+            var pathList = f.Select(e => e.Keys.ToArray()).ToArray();
             var level = new int[N];
             Func<bool> reset = () =>
             {
@@ -88,24 +89,31 @@ namespace Library
                 return true;
             };
             Func<int, long, long> dfs = null;
+            var itr = new int[N];
             dfs = (vtx, c) =>
             {
                 if (vtx == t) return c;
-                foreach (var item in f[vtx])
+                for (; itr[vtx] < pathList[vtx].Length; ++itr[vtx])
                 {
-                    if (item.Value > 0 && level[vtx] < level[item.Key])
+                    var pathInfo = pathList[vtx][itr[vtx]];
+                    var cap = f[vtx][pathInfo];
+                    if (cap > 0 && level[vtx] < level[pathInfo])
                     {
-                        var flow = dfs(item.Key, Min(c, item.Value));
+                        var flow = dfs(pathInfo, Min(c, cap));
                         if (flow == 0) continue;
-                        f[vtx][item.Key] -= flow;
-                        f[item.Key][vtx] += flow;
+                        f[vtx][pathInfo] -= flow;
+                        f[pathInfo][vtx] += flow;
                         return flow;
                     }
                 }
                 return 0;
             };
             var sum = 0L;
-            while (reset() && level[t] > 0) for (long flow; (flow = dfs((int)s, long.MaxValue)) > 0;) sum += flow;
+            while (reset() && level[t] > 0)
+            {
+                itr = new int[N];
+                for (long flow; (flow = dfs((int)s, long.MaxValue)) > 0;) sum += flow;
+            }
             for (var i = 0; i < path.Length; i++) foreach (var to in path[i]) f[i][to.Key] = to.Value - f[i][to.Key];
             return new FlowResult { maxflow = sum, fromToCost = f };
         }
