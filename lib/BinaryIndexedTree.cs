1855722aff3080af6161a0989775c424d98b39d2
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-09-12 03:26:05 +0900

    update lib: indexはint

--- lib/BinaryIndexedTree.cs
+++ lib/BinaryIndexedTree.cs
@@ -32,21 +32,23 @@ namespace Library
         public long Sum(long idx)
         {
             var ret = 0L;
-            while (idx > 0)
+            var i = (int)idx;
+            while (i > 0)
             {
-                ret += dat[idx];
-                idx -= idx & -idx;
+                ret += dat[i];
+                i -= i & -i;
             }
             return ret;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Add(long idx, long value)
         {
-            if (idx <= 0) throw new Exception("1-indexed");
-            while (idx <= n)
+            var i = (int)idx;
+            if (i <= 0) throw new Exception("1-indexed");
+            while (i <= n)
             {
-                dat[idx] += value;
-                idx += idx & -idx;
+                dat[i] += value;
+                i += i & -i;
             }
         }
     }
