8a3debf19a9cba47b4d7acac45980e4a9901e41b
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-09-17 01:56:02 +0900

    update lib: 高速化

--- lib/Mod/NTT.cs
+++ lib/Mod/NTT.cs
@@ -22,22 +22,6 @@ namespace Library
         const uint mod4 = 998244353;
         const uint root4 = 3;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint add1(uint x, uint y) => (x + y < mod1) ? (x + y) : (x + y - mod1);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint add2(uint x, uint y) => (x + y < mod2) ? (x + y) : (x + y - mod2);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint add3(uint x, uint y) => (x + y < mod3) ? (x + y) : (x + y - mod3);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint add4(uint x, uint y) => (x + y < mod4) ? (x + y) : (x + y - mod4);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint sub1(uint x, uint y) => (x >= y) ? (x - y) : (mod1 - y + x);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint sub2(uint x, uint y) => (x >= y) ? (x - y) : (mod2 - y + x);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint sub3(uint x, uint y) => (x >= y) ? (x - y) : (mod3 - y + x);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static uint sub4(uint x, uint y) => (x >= y) ? (x - y) : (mod4 - y + x);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static uint mul1(uint x, uint y) => (uint)((ulong)x * y % mod1);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static uint mul2(uint x, uint y) => (uint)((ulong)x * y % mod2);
@@ -62,38 +46,40 @@ namespace Library
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static uint inverse4(uint x) { long b = mod4, r = 1, u = 0, t = 0, x2 = x; while (b > 0) { var q = x2 / b; t = u; u = r - q * u; r = t; t = b; b = x2 - q * b; x2 = t; } return (uint)(r < 0 ? r + mod4 : r); }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void ntt1(ref uint[] a, bool rev = false) { var n = (uint)a.Length; if (n == 1) return; var b = new uint[n]; var s = pow1(root1, rev ? (mod1 - 1 - (mod1 - 1) / n) : (mod1 - 1) / n); var kp = Enumerable.Repeat((uint)1, (int)(n / 2 + 1)).ToArray(); uint i, j, k, l, r; for (i = 0; i < n / 2; ++i) kp[i + 1] = mul1(kp[i], s); for (i = 1, l = n / 2; i < n; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var p = a[k + r]; var q = a[k + r + n / 2]; b[k + 2 * r] = add1(p, q); b[k + 2 * r + i] = mul1(sub1(p, q), s); } } var t = a; a = b; b = t; } if (rev) { s = inverse1(n); for (i = 0; i < n; ++i) a[i] = mul1(a[i], s); } }
+        static void ntt1(ref uint[] a, bool rev = false) { if (a.Length == 1) return; var halfn = (uint)a.Length >> 1; var b = new uint[a.Length]; var s = pow1(root1, rev ? (mod1 - 1 - (mod1 - 1) / (uint)a.Length) : (mod1 - 1) / (uint)a.Length); uint i, j, k, l, r; var kp = new uint[halfn + 1]; kp[0] = 1; for (i = 0; i < kp.Length - 1; ++i) kp[i + 1] = mul1(kp[i], s); for (i = 1, l = halfn; i < a.Length; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var kr = k + r; var krhalfn = kr + halfn; b[kr + r] = (a[kr] + a[krhalfn]) % mod1; b[kr + r + i] = (uint)(((ulong)(a[kr] + mod1 - a[krhalfn]) * s) % mod1); } } var t = a; a = b; b = t; } if (rev) { s = inverse1((uint)a.Length); for (i = 0; i < a.Length; ++i) a[i] = mul1(a[i], s); } }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void ntt2(ref uint[] a, bool rev = false) { var n = (uint)a.Length; if (n == 1) return; var b = new uint[n]; var s = pow2(root2, rev ? (mod2 - 1 - (mod2 - 1) / n) : (mod2 - 1) / n); var kp = Enumerable.Repeat((uint)1, (int)(n / 2 + 1)).ToArray(); uint i, j, k, l, r; for (i = 0; i < n / 2; ++i) kp[i + 1] = mul2(kp[i], s); for (i = 1, l = n / 2; i < n; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var p = a[k + r]; var q = a[k + r + n / 2]; b[k + 2 * r] = add2(p, q); b[k + 2 * r + i] = mul2(sub2(p, q), s); } } var t = a; a = b; b = t; } if (rev) { s = inverse2(n); for (i = 0; i < n; ++i) a[i] = mul2(a[i], s); } }
+        static void ntt2(ref uint[] a, bool rev = false) { if (a.Length == 1) return; var halfn = (uint)a.Length >> 1; var b = new uint[a.Length]; var s = pow2(root2, rev ? (mod2 - 1 - (mod2 - 1) / (uint)a.Length) : (mod2 - 1) / (uint)a.Length); uint i, j, k, l, r; var kp = new uint[halfn + 1]; kp[0] = 1; for (i = 0; i < kp.Length - 1; ++i) kp[i + 1] = mul2(kp[i], s); for (i = 1, l = halfn; i < a.Length; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var kr = k + r; var krhalfn = kr + halfn; b[kr + r] = (a[kr] + a[krhalfn]) % mod2; b[kr + r + i] = (uint)(((ulong)(a[kr] + mod2 - a[krhalfn]) * s) % mod2); } } var t = a; a = b; b = t; } if (rev) { s = inverse2((uint)a.Length); for (i = 0; i < a.Length; ++i) a[i] = mul2(a[i], s); } }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void ntt3(ref uint[] a, bool rev = false) { var n = (uint)a.Length; if (n == 1) return; var b = new uint[n]; var s = pow3(root3, rev ? (mod3 - 1 - (mod3 - 1) / n) : (mod3 - 1) / n); var kp = Enumerable.Repeat((uint)1, (int)(n / 2 + 1)).ToArray(); uint i, j, k, l, r; for (i = 0; i < n / 2; ++i) kp[i + 1] = mul3(kp[i], s); for (i = 1, l = n / 2; i < n; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var p = a[k + r]; var q = a[k + r + n / 2]; b[k + 2 * r] = add3(p, q); b[k + 2 * r + i] = mul3(sub3(p, q), s); } } var t = a; a = b; b = t; } if (rev) { s = inverse3(n); for (i = 0; i < n; ++i) a[i] = mul3(a[i], s); } }
+        static void ntt3(ref uint[] a, bool rev = false) { if (a.Length == 1) return; var halfn = (uint)a.Length >> 1; var b = new uint[a.Length]; var s = pow3(root3, rev ? (mod3 - 1 - (mod3 - 1) / (uint)a.Length) : (mod3 - 1) / (uint)a.Length); uint i, j, k, l, r; var kp = new uint[halfn + 1]; kp[0] = 1; for (i = 0; i < kp.Length - 1; ++i) kp[i + 1] = mul3(kp[i], s); for (i = 1, l = halfn; i < a.Length; i <<= 1, l >>= 1) { for (j = 0, r = 0; j < l; ++j, r += i) { for (k = 0, s = kp[i * j]; k < i; ++k) { var kr = k + r; var krhalfn = kr + halfn; b[kr + r] = (a[kr] + a[krhalfn]) % mod3; b[kr + r + i] = (uint)(((ulong)(a[kr] + mod3 - a[krhalfn]) * s) % mod3); } } var t = a; a = b; b = t; } if (rev) { s = inverse3((uint)a.Length); for (i = 0; i < a.Length; ++i) a[i] = mul3(a[i], s); } }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static void ntt4(ref uint[] a, bool rev = false)
         {
-            var n = (uint)a.Length;
-            if (n == 1) return;
-            var b = new uint[n];
-            var s = pow4(root4, rev ? (mod4 - 1 - (mod4 - 1) / n) : (mod4 - 1) / n);
-            var kp = Enumerable.Repeat((uint)1, (int)(n / 2 + 1)).ToArray();
+            if (a.Length == 1) return;
+            var halfn = (uint)a.Length >> 1;
+            var b = new uint[a.Length];
+            var s = pow4(root4, rev ? (mod4 - 1 - (mod4 - 1) / (uint)a.Length) : (mod4 - 1) / (uint)a.Length);
             uint i, j, k, l, r;
-            for (i = 0; i < n / 2; ++i) kp[i + 1] = mul4(kp[i], s);
-            for (i = 1, l = n / 2; i < n; i <<= 1, l >>= 1)
+            var kp = new uint[halfn + 1];
+            kp[0] = 1;
+            for (i = 0; i < kp.Length - 1; ++i) kp[i + 1] = mul4(kp[i], s);
+            for (i = 1, l = halfn; i < a.Length; i <<= 1, l >>= 1)
             {
                 for (j = 0, r = 0; j < l; ++j, r += i)
                 {
                     for (k = 0, s = kp[i * j]; k < i; ++k)
                     {
-                        var p = a[k + r]; var q = a[k + r + n / 2];
-                        b[k + 2 * r] = add4(p, q);
-                        b[k + 2 * r + i] = mul4(sub4(p, q), s);
+                        var kr = k + r;
+                        var krhalfn = kr + halfn;
+                        b[kr + r] = (a[kr] + a[krhalfn]) % mod4;
+                        b[kr + r + i] = (uint)(((ulong)(a[kr] + mod4 - a[krhalfn]) * s) % mod4);
                     }
                 }
                 var t = a; a = b; b = t;
             }
             if (rev)
             {
-                s = inverse4(n);
-                for (i = 0; i < n; ++i) a[i] = mul4(a[i], s);
+                s = inverse4((uint)a.Length);
+                for (i = 0; i < a.Length; ++i) a[i] = mul4(a[i], s);
             }
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -133,8 +119,8 @@ namespace Library
             var ret = new long[x.Length];
             for (var i = 0; i < x.Length; ++i)
             {
-                var v1 = mul2(sub2(add2(y[i], mod2), x[i]), m1_inv_m2);
-                var v2 = mul3(sub3(sub3(add3(z[i], mod3), x[i]), mul3(mod1, v1)), m12_inv_m3);
+                var v1 = mul2(y[i] + (mod2 << 1) - x[i], m1_inv_m2);
+                var v2 = mul3(z[i] + (mod3 << 1) - x[i] + mod3 - mul3(mod1, v1), m12_inv_m3);
                 ret[i] = (x[i] + ((long)mod1 * v1 % mod) + ((long)m12_mod * v2 % mod)) % mod;
             }
             return ret;
