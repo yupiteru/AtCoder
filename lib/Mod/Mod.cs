1b695524396859ce760bfeefce7d1e5fc132c9ef
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-10-17 01:21:02 +0900

    update lib: Log追加

--- lib/Mod/Mod.cs
+++ lib/Mod/Mod.cs
@@ -119,6 +119,50 @@ namespace Library
             }
             return a;
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public long Log(long a, long b)
+        {
+            var g = 1L;
+            for (var i = _mod; i != 0; i >>= 1)
+            {
+                g *= a;
+                g %= _mod;
+            }
+            g = LIB_Math.GCD(g, _mod);
+            var t = 1L;
+            var c = 0L;
+            for (; t % g != 0; ++c)
+            {
+                if (t == b) return c;
+                t *= a;
+                t %= _mod;
+            }
+            if (b % g != 0) return -1;
+            t /= g;
+            b /= g;
+            var n = _mod / g;
+            var h = 0L;
+            var gs = 1L;
+            for (; h * h < n; ++h)
+            {
+                gs *= a;
+                gs %= n;
+            }
+            var bs = new Dictionary<long, long>();
+            for (long s = 0L, e = b; s < h; bs[e] = ++s)
+            {
+                e *= a;
+                e %= n;
+            }
+            for (long s = 0L, e = t; s < n;)
+            {
+                e *= gs;
+                e %= n;
+                s += h;
+                if (bs.ContainsKey(e) && bs[e] > 0) return c + s - bs[e];
+            }
+            return -1;
+        }
     }
     ////end
 }
