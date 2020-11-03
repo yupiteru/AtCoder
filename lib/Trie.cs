a7729e50f5a7c07bf932a5967bcb6a10b2677d74
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2020-11-03 16:41:29 +0900

    update lib: Add AhoCrasick

--- lib/Trie.cs
+++ lib/Trie.cs
@@ -16,64 +16,111 @@ namespace Library
         class TrieNode
         {
             public Dictionary<T, TrieNode> Next;
+            public TrieNode fail;
             public List<int> Accept;
+            public HashSet<int> UnionAccept;
             public int Count;
             public TrieNode()
             {
                 Next = new Dictionary<T, TrieNode>();
                 Accept = new List<int>();
+                UnionAccept = new HashSet<int>();
             }
         }
+        bool builtAhoCorasick;
+        List<int> patLens;
         TrieNode root;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public LIB_Trie()
         {
             root = new TrieNode();
+            builtAhoCorasick = false;
+            patLens = new List<int>();
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Add(IEnumerable<T> str)
+        public IEnumerable<(int patIdx, int pos)> AhoCorasick(ReadOnlySpan<T> str) => AhoCorasick((IEnumerable<T>)str.ToArray());
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public IEnumerable<(int patIdx, int pos)> AhoCorasick(IEnumerable<T> str)
         {
-            var id = root.Count;
-            var node = root;
-            foreach (var item in str)
+            if (!builtAhoCorasick)
             {
-                ++node.Count;
-                TrieNode nextNode;
-                if (!node.Next.TryGetValue(item, out nextNode)) nextNode = node.Next[item] = new TrieNode();
-                node = nextNode;
+                builtAhoCorasick = true;
+                var q = new Queue<TrieNode>();
+                foreach (var item in root.Next)
+                {
+                    item.Value.fail = root;
+                    foreach (var item2 in item.Value.Accept) item.Value.UnionAccept.Add(item2);
+                    q.Enqueue(item.Value);
+                }
+                root.fail = root;
+                while (q.Count > 0)
+                {
+                    var t = q.Dequeue();
+                    foreach (var item in t.Next)
+                    {
+                        foreach (var item2 in item.Value.Accept) item.Value.UnionAccept.Add(item2);
+                        q.Enqueue(item.Value);
+                        var r = t.fail;
+                        while (r != root && !r.Next.ContainsKey(item.Key)) r = r.fail;
+                        if (!r.Next.TryGetValue(item.Key, out item.Value.fail))
+                        {
+                            item.Value.fail = root;
+                        }
+                        foreach (var item2 in item.Value.fail.UnionAccept)
+                        {
+                            item.Value.UnionAccept.Add(item2);
+                        }
+                    }
+                }
             }
-            ++node.Count;
-            node.Accept.Add(id);
+            var v = root;
+            var pos = 0;
+            foreach (var item in str.ToArray())
+            {
+                ++pos;
+                while (v != root && !v.Next.ContainsKey(item)) v = v.fail;
+                if (!v.Next.TryGetValue(item, out v)) v = root;
+                foreach (var item2 in v.UnionAccept) yield return (item2, pos - patLens[item2]);
+            }
+            yield break;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Add(ReadOnlySpan<T> str)
         {
+            builtAhoCorasick = false;
             var id = root.Count;
             var node = root;
+            var len = 0;
             foreach (var item in str)
             {
+                ++len;
                 ++node.Count;
                 TrieNode nextNode;
                 if (!node.Next.TryGetValue(item, out nextNode)) nextNode = node.Next[item] = new TrieNode();
                 node = nextNode;
             }
+            patLens.Add(len);
             ++node.Count;
             node.Accept.Add(id);
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public List<int> Query(IEnumerable<T> str)
+        public void Add(IEnumerable<T> str)
         {
-            var ret = new List<int>();
+            builtAhoCorasick = false;
+            var id = root.Count;
             var node = root;
+            var len = 0;
             foreach (var item in str)
             {
-                foreach (var item2 in node.Accept) ret.Add(item2);
+                ++len;
+                ++node.Count;
                 TrieNode nextNode;
-                if (!node.Next.TryGetValue(item, out nextNode)) return ret;
+                if (!node.Next.TryGetValue(item, out nextNode)) nextNode = node.Next[item] = new TrieNode();
                 node = nextNode;
             }
-            foreach (var item in node.Accept) ret.Add(item);
-            return ret;
+            patLens.Add(len);
+            ++node.Count;
+            node.Accept.Add(id);
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public List<int> Query(ReadOnlySpan<T> str)
@@ -91,16 +138,19 @@ namespace Library
             return ret;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int[] Find(IEnumerable<T> str)
+        public List<int> Query(IEnumerable<T> str)
         {
+            var ret = new List<int>();
             var node = root;
             foreach (var item in str)
             {
+                foreach (var item2 in node.Accept) ret.Add(item2);
                 TrieNode nextNode;
-                if (!node.Next.TryGetValue(item, out nextNode)) return new int[0];
+                if (!node.Next.TryGetValue(item, out nextNode)) return ret;
                 node = nextNode;
             }
-            return node.Accept.ToArray();
+            foreach (var item in node.Accept) ret.Add(item);
+            return ret;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int[] Find(ReadOnlySpan<T> str)
@@ -115,9 +165,21 @@ namespace Library
             return node.Accept.ToArray();
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int[] Find(IEnumerable<T> str)
+        {
+            var node = root;
+            foreach (var item in str)
+            {
+                TrieNode nextNode;
+                if (!node.Next.TryGetValue(item, out nextNode)) return new int[0];
+                node = nextNode;
+            }
+            return node.Accept.ToArray();
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int Count() => root.Count;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int Count(IEnumerable<T> prefix)
+        public int Count(ReadOnlySpan<T> prefix)
         {
             var node = root;
             foreach (var item in prefix)
@@ -129,7 +191,7 @@ namespace Library
             return node.Count;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int Count(ReadOnlySpan<T> prefix)
+        public int Count(IEnumerable<T> prefix)
         {
             var node = root;
             foreach (var item in prefix)
