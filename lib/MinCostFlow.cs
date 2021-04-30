20b2206fc5d618c1e159f5d3603a9d10dca6a8d2
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2021-04-30 21:51:30 +0900

    update lib: MinCostFlow

--- lib/MinCostFlow.cs
+++ lib/MinCostFlow.cs
@@ -29,53 +29,91 @@ namespace Library
         const int SHIFT_SIZE = 30;
         const int MASK = 1073741823;
         List<ulong> pos;
-        List<int>[] gTo;
-        List<int>[] gRev;
-        List<long>[] gCap;
-        List<long>[] gCost;
+        int[][] gTo;
+        int[][] gRev;
+        long[][] gCap;
+        long[][] gCost;
+        int[] gTolen;
+        int[] gRevlen;
+        int[] gCaplen;
+        int[] gCostlen;
         int N;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public LIB_MinCostFlow(long n)
         {
             N = (int)n;
             pos = new List<ulong>();
-            gTo = new List<int>[n];
-            gRev = new List<int>[n];
-            gCap = new List<long>[n];
-            gCost = new List<long>[n];
+            gTo = new int[n][];
+            gRev = new int[n][];
+            gCap = new long[n][];
+            gCost = new long[n][];
+            gTolen = new int[n];
+            gRevlen = new int[n];
+            gCaplen = new int[n];
+            gCostlen = new int[n];
             for (var i = 0; i < n; i++)
             {
-                gTo[i] = new List<int>();
-                gRev[i] = new List<int>();
-                gCap[i] = new List<long>();
-                gCost[i] = new List<long>();
+                gTo[i] = new int[8];
+                gRev[i] = new int[8];
+                gCap[i] = new long[8];
+                gCost[i] = new long[8];
             }
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int AddEdge(long from, long to, long cap, long cost)
         {
             var ret = pos.Count;
-            pos.Add(((ulong)from << SHIFT_SIZE) | (uint)gTo[from].Count);
-            gTo[from].Add((int)to);
-            gRev[from].Add(gRev[to].Count);
-            gCap[from].Add(cap);
-            gCost[from].Add(cost);
-            gTo[to].Add((int)from);
-            gRev[to].Add(gRev[from].Count - 1);
-            gCap[to].Add(0);
-            gCost[to].Add(-cost);
+            pos.Add(((ulong)from << SHIFT_SIZE) | (uint)gTolen[from]);
+            if (gTo[from].Length == gTolen[from])
+            {
+                var tmp = new int[gTolen[from] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<int, byte>(ref tmp[0]), ref Unsafe.As<int, byte>(ref gTo[from][0]), (uint)(gTolen[from] << 2));
+                gTo[from] = tmp;
+                tmp = new int[gRevlen[from] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<int, byte>(ref tmp[0]), ref Unsafe.As<int, byte>(ref gRev[from][0]), (uint)(gRevlen[from] << 2));
+                gRev[from] = tmp;
+                var tmp2 = new long[gCaplen[from] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<long, byte>(ref tmp2[0]), ref Unsafe.As<long, byte>(ref gCap[from][0]), (uint)(gCaplen[from] << 3));
+                gCap[from] = tmp2;
+                tmp2 = new long[gCostlen[from] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<long, byte>(ref tmp2[0]), ref Unsafe.As<long, byte>(ref gCost[from][0]), (uint)(gCostlen[from] << 3));
+                gCost[from] = tmp2;
+            }
+            if (gTo[to].Length == gTolen[to])
+            {
+                var tmp = new int[gTolen[to] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<int, byte>(ref tmp[0]), ref Unsafe.As<int, byte>(ref gTo[to][0]), (uint)(gTolen[to] << 2));
+                gTo[to] = tmp;
+                tmp = new int[gRevlen[to] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<int, byte>(ref tmp[0]), ref Unsafe.As<int, byte>(ref gRev[to][0]), (uint)(gRevlen[to] << 2));
+                gRev[to] = tmp;
+                var tmp2 = new long[gCaplen[to] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<long, byte>(ref tmp2[0]), ref Unsafe.As<long, byte>(ref gCap[to][0]), (uint)(gCaplen[to] << 3));
+                gCap[to] = tmp2;
+                tmp2 = new long[gCostlen[to] << 1];
+                Unsafe.CopyBlock(ref Unsafe.As<long, byte>(ref tmp2[0]), ref Unsafe.As<long, byte>(ref gCost[to][0]), (uint)(gCostlen[to] << 3));
+                gCost[to] = tmp2;
+            }
+            gTo[from][gTolen[from]++] = (int)to;
+            gRev[from][gRevlen[from]++] = gRevlen[to];
+            gCap[from][gCaplen[from]++] = cap;
+            gCost[from][gCostlen[from]++] = cost;
+            gTo[to][gTolen[to]++] = (int)from;
+            gRev[to][gRevlen[to]++] = gRevlen[from] - 1;
+            gCap[to][gCaplen[to]++] = 0;
+            gCost[to][gCostlen[to]++] = -cost;
             return ret;
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Edge GetEdge(long i)
         {
-            var idx = (int)i;
-            var idxf = pos[idx] >> SHIFT_SIZE;
-            var idxt = (int)(pos[idx] & MASK);
+            var posidx = pos[(int)i];
+            var idxf = posidx >> SHIFT_SIZE;
+            var idxt = (int)(posidx & MASK);
             var eTo = gTo[idxf][idxt];
             var eRev = gRev[idxf][idxt];
             var reCap = gCap[eTo][eRev];
-            return new Edge { from = (int)(pos[idx] >> SHIFT_SIZE), to = eTo, cap = gCap[idxf][idxt] + reCap, flow = reCap, cost = gCost[idxf][idxt] };
+            return new Edge { from = (int)(idxf), to = eTo, cap = gCap[idxf][idxt] + reCap, flow = reCap, cost = gCost[idxf][idxt] };
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Edge[] GetAllEdge()
@@ -90,66 +128,72 @@ namespace Library
         public CapCost Flow(long s, long t, long flowLimit) => Slope(s, t, flowLimit).Last();
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public List<CapCost> Slope(long s, long t) => Slope(s, t, long.MaxValue >> 2);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         public List<CapCost> Slope(long s, long t, long flowLimit)
         {
-            var dual = new long[N];
-            var dist = new long[N];
-            var pv = new int[N];
-            var pe = new int[N];
+            var tmp1 = new long[N << 1];
+            var tmp2 = new int[N];
+            var tmp3 = new int[N];
+            ref var dualref = ref tmp1[0];
+            ref var distref = ref tmp1[N];
+            ref var pvref = ref tmp2[0];
+            ref var peref = ref tmp3[0];
             var flow = 0L;
             var cost = 0L;
             var prevCost = -1L;
             var result = new List<CapCost>();
             result.Add(new CapCost { cap = flow, cost = cost });
+            var vis = new bool[N];
+            ref var visref = ref vis[0];
             while (flow < flowLimit)
             {
-                for (var i = 0; i < dist.Length; ++i) dist[i] = long.MaxValue >> 2;
-                for (var i = 0; i < pv.Length; ++i) pv[i] = -1;
-                for (var i = 0; i < pe.Length; ++i) pe[i] = -1;
-                var vis = new bool[N];
+                Unsafe.InitBlock(ref Unsafe.As<int, byte>(ref pvref), 0xFF, (uint)(N << 2));
+                Unsafe.InitBlock(ref Unsafe.As<int, byte>(ref peref), 0xFF, (uint)(N << 2));
+                Unsafe.InitBlock(ref Unsafe.As<long, byte>(ref distref), 0x7F, (uint)(N << 3));
+                Unsafe.InitBlock(ref Unsafe.As<bool, byte>(ref visref), 0, (uint)N);
                 var que = new LIB_PriorityQueue();
-                dist[s] = 0;
+                Unsafe.Add(ref distref, (int)s) = 0;
                 que.Push(0, (int)s);
                 while (que.Count > 0)
                 {
                     var v = que.Pop().Value;
-                    if (vis[v]) continue;
-                    vis[v] = true;
+                    if (Unsafe.Add(ref visref, v)) continue;
+                    Unsafe.Add(ref visref, v) = true;
                     if (v == t) break;
                     var gToV = gTo[v];
+                    var gToVlen = gTolen[v];
                     var gCapV = gCap[v];
                     var gCostV = gCost[v];
-                    var dualv = dual[v];
-                    var distv = dist[v];
-                    for (var i = 0; i < gToV.Count; ++i)
+                    var dualv = Unsafe.Add(ref dualref, v);
+                    var distv = Unsafe.Add(ref distref, v);
+                    for (var i = 0; i < gToVlen; ++i)
                     {
                         var eTo = gToV[i];
-                        if (vis[eTo] || gCapV[i] == 0) continue;
-                        var thisCost = gCostV[i] - dual[eTo] + dualv;
-                        if (dist[eTo] - distv > thisCost)
+                        if (Unsafe.Add(ref visref, eTo) || gCapV[i] == 0) continue;
+                        var thisCost = gCostV[i] - Unsafe.Add(ref dualref, eTo) + dualv;
+                        if (Unsafe.Add(ref distref, eTo) - distv > thisCost)
                         {
-                            dist[eTo] = distv + thisCost;
-                            pv[eTo] = v;
-                            pe[eTo] = i;
-                            que.Push(dist[eTo], eTo);
+                            Unsafe.Add(ref distref, eTo) = distv + thisCost;
+                            Unsafe.Add(ref pvref, eTo) = v;
+                            Unsafe.Add(ref peref, eTo) = i;
+                            que.Push(Unsafe.Add(ref distref, eTo), eTo);
                         }
                     }
                 }
-                if (!vis[t]) break;
+                if (!Unsafe.Add(ref visref, (int)t)) break;
                 for (var v = 0; v < vis.Length; ++v)
                 {
-                    if (!vis[v]) continue;
-                    dual[v] -= dist[t] - dist[v];
+                    if (!Unsafe.Add(ref visref, v)) continue;
+                    Unsafe.Add(ref dualref, v) -= Unsafe.Add(ref distref, (int)t) - Unsafe.Add(ref distref, v);
                 }
                 var c = flowLimit - flow;
-                for (var v = t; v != s; v = pv[v]) c = Math.Min(c, gCap[pv[v]][pe[v]]);
-                for (var v = t; v != s; v = pv[v])
+                for (var v = (int)t; v != s; v = Unsafe.Add(ref pvref, v)) c = Math.Min(c, gCap[Unsafe.Add(ref pvref, (int)v)][Unsafe.Add(ref peref, v)]);
+                for (var v = (int)t; v != s; v = Unsafe.Add(ref pvref, v))
                 {
-                    gCap[pv[v]][pe[v]] -= c;
-                    gCap[v][gRev[pv[v]][pe[v]]] += c;
+                    gCap[Unsafe.Add(ref pvref, (int)v)][Unsafe.Add(ref peref, v)] -= c;
+                    gCap[v][gRev[Unsafe.Add(ref pvref, (int)v)][Unsafe.Add(ref peref, v)]] += c;
                 }
-                var d = -dual[s];
+                var d = -Unsafe.Add(ref dualref, (int)s);
                 flow += c;
                 cost += c * d;
                 if (prevCost == d) result.RemoveAt(result.Count - 1);
