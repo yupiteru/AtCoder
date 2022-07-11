63241ebdfdcd9f62de40acce67b7f8d6cf7c28fb
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2022-07-12 00:00:24 +0900

    update lib: BitUtil 高速化

--- lib/BitUtil.cs
+++ lib/BitUtil.cs
@@ -38,13 +38,7 @@ namespace Library
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static public long PopCount(ulong value)
         {
-            value = (value & 0x5555555555555555) + ((value >> 1) & 0x5555555555555555);
-            value = (value & 0x3333333333333333) + ((value >> 2) & 0x3333333333333333);
-            value = (value & 0x0f0f0f0f0f0f0f0f) + ((value >> 4) & 0x0f0f0f0f0f0f0f0f);
-            value = (value & 0x00ff00ff00ff00ff) + ((value >> 8) & 0x00ff00ff00ff00ff);
-            value = (value & 0x0000ffff0000ffff) + ((value >> 16) & 0x0000ffff0000ffff);
-            value = (value & 0x00000000ffffffff) + ((value >> 32) & 0x00000000ffffffff);
-            return (long)value;
+            return (long)System.Runtime.Intrinsics.X86.Popcnt.X64.PopCount(value);
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static public long PopCount(long value) => PopCount((ulong)value);
