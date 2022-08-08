48fcd116724122c7c7de4386b8b53fe1ec7544b0
Yupiteru <1593516+yupiteru@users.noreply.github.com>
2022-08-08 17:06:30 +0900

    update lib: 三分探索

--- lib/Common.cs
+++ lib/Common.cs
@@ -66,7 +66,7 @@ namespace Library
             }
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static public (int[] result, List<T> dp) LIS<T>(IEnumerable<T> array, bool strict) where T : IComparable
+        static public (int[] result, List<T> dp) LIS<T>(IEnumerable<T> array, bool strict) where T : IComparable<T>
         {
             var l = new List<T>();
             var ret = new List<int>();
@@ -85,6 +85,85 @@ namespace Library
                 ret.Add(l.Count);
             }
             return (ret.ToArray(), l);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public (long k, T val) SanbunTansaku<T>(long left, long right, Func<long, T> fun) where T : IComparable<T>
+        {
+            var size = right - left;
+            var fib = new LIB_Deque<long>();
+            fib.PushBack(1);
+            fib.PushBack(1);
+            while (fib[fib.Count - 1] * 2 + fib[fib.Count - 2] <= size)
+            {
+                fib.PushBack(fib[fib.Count - 1] + fib[fib.Count - 2]);
+            }
+            var valueLeft = left;
+            var valueRight = right;
+            Func<long, (bool isMax, T value)> calc = x =>
+            {
+                if (x < valueLeft || valueRight <= x) return (true, default(T));
+                return (false, fun(x));
+            };
+            --left;
+            var lv = calc(left);
+            var m1 = left + fib.PopBack();
+            var m2 = m1 + fib.Back;
+            var m1v = calc(m1);
+            var m2v = calc(m2);
+            var rv = calc(right = m1 - left + m1 + fib.Back);
+            while (fib.Count > 2)
+            {
+                if (m1v.CompareTo(m2v) < 0)
+                {
+                    rv = m2v; right = m2;
+                    m2v = m1v; m2 = m1;
+                    m1v = calc(m1 = left + fib.PopBack());
+                }
+                else
+                {
+                    lv = m1v; left = m1;
+                    m1v = m2v; m1 = m2;
+                    m2v = calc(m2 = left + fib.PopBack() + fib.Back);
+                }
+            }
+            var ans = calc(left);
+            for (var i = left + 1; i < right; ++i)
+            {
+                var na = calc(i);
+                if (ans.CompareTo(na) > 0)
+                {
+                    ans = na;
+                    left = i;
+                }
+            }
+            return (left, ans.value);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static public (double k, T val) SanbunTansaku<T>(double left, double right, Func<double, T> fun) where T : IComparable<T>
+        {
+            const double PHI = 0.6180339887;
+            var lv = fun(left);
+            var rv = fun(right);
+            var m2 = left + (right - left) * PHI;
+            var m2v = fun(m2);
+            var m1 = left + (m2 - left) * PHI;
+            var m1v = fun(m1);
+            for (var i = 0; i < 200; ++i)
+            {
+                if (m1v.CompareTo(m2v) < 0)
+                {
+                    rv = m2v; right = m2;
+                    m2v = m1v; m2 = m1;
+                    m1v = fun(m1 = left + (m2 - left) * PHI);
+                }
+                else
+                {
+                    lv = m1v; left = m1;
+                    m1v = m2v; m1 = m2;
+                    m2v = fun(m2 = m1 + (m1 - left) * PHI);
+                }
+            }
+            return (m1, m1v);
         }
     }
     ////end
